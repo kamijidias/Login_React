2c28893fabf16daf11d34a12e909cff26eb1de2e
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-04 20:11:47 -0300

    Primeira etapa do projeto

---
+++ project/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Private } from './pages/Private';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Header do site</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Página privada</Link>
+        </nav>
+      </header>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+//32/28
